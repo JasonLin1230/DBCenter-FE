af774ad14f6ffcc9d0939ca09bcff9fe3d94a1cb
wangruozheng <wangruozheng@zhizhangyi.com>
2018-04-20 18:23:13 +0800

    vue-router配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-import index from '@/pages/index'
-import login from '@/pages/login'
+import index from '@/pages/index';
+import login from '@/pages/login';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
 	routes: [
